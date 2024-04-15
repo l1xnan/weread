679284c30b47dafb590c55f4e5a51c76ee874bfa
l1xnan <l1xnan@qq.com>
2024-04-15 10:21:53 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/primitives";
+import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
